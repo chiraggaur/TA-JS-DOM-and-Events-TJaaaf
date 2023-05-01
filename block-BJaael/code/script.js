7ad3ee260a56a24a41fd9b723ce49684a2aa206d
chirag gaur <chiragwalker@gmail.com>
2023-05-01 09:51:12 +0530

    disco grid completed

--- block-BJaael/code/script.js
+++ block-BJaael/code/script.js
@@ -1,47 +1,50 @@
-let outerBox = document.querySelector('.masterBox');
-function random(){
-    let list = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'];
-    let color = '#';
-    
-    for (let i = 0; i < 6; i++){
-        let random = Math.floor(Math.random()*16);
-        color = color + list[random];
-    }
+// create 500 boxes with width and height 4rem 
 
-    return color;
+let outerBox = document.querySelector('.container');
+for(let i = 0; i < 500; i++ ){
+    let div = document.createElement('div');
+    div.classList.add('boxes');
+    let h3 = document.createElement('h3');
+     h3.innerText = randomNumber(500);
+     div.append(h3);
+    //  console.log(div);
+     outerBox.append(div); 
 }
 
+// random color generator
+
+function colorGenerator(){
+    let list = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'];
+    let colorCode = '#';
 
 
+    for(let i = 0; i < 6; i++){
+       random = Math.floor(Math.random() * 16);
+        colorCode = colorCode + list[random];
+        
+    }
+     return colorCode;
+    // console.log(colorCode);
+}
+   let innerBox = document.querySelectorAll('.boxes');
+   function action(){
+    innerBox.forEach((elm) => {
+       elm.style.backgroundColor = colorGenerator();
+       elm.querySelector('h3').innerText = randomNumber(500);
+    });
 
-outerBox.addEventListener('mousemove',function(){
-     outerBox.style.backgroundColor = random();
-});
+     
+}
 
-let gridNumber1 = document.querySelector('.number1');
-gridNumber1.addEventListener('mousemove',function(){
-    let random = Math.floor(Math.random()*500);
-    let recurringNumber = random;
-    return gridNumber1.innerHTML = recurringNumber ;
-})
+  // add event lsitener
 
- let gridNumber2 = document.querySelector('.number2');
- gridNumber2.addEventListener('mousemove',function(){
-    let random = Math.floor(Math.random()*500);
-    let recurringNumber = random;
-    return gridNumber2.innerHTML = recurringNumber ;
-})
+  outerBox.addEventListener('mousemove',action);
 
- let gridNumber3 = document.querySelector('.number3');
- gridNumber3.addEventListener('mousemove',function(){
-    let random = Math.floor(Math.random()*500);
-    let recurringNumber = random;
-    return gridNumber3.innerHTML = recurringNumber ;
-})
+  //random No
 
- let gridNumber4 = document.querySelector('.number4');
- gridNumber4.addEventListener('mousemove',function(){
-    let random = Math.floor(Math.random()*500);
-    let recurringNumber = random;
-    return gridNumber4.innerHTML = recurringNumber ;
-})
+  function randomNumber(max){
+
+    let number = Math.floor(Math.random() * max);
+
+    return number;
+ }
